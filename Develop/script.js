b10203316e44a299e1e99caa4de9bc307ec92245
Chris Papanicholas JR <cpapanicholas@gmail.com>
2023-09-28 20:16:41 -0500

    Reorganized code for js

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  $(".saveBtn").on("click", function () {
-
-  });
-
-  var currentDate = dayjs().format("MMMM D, YYYY");
-  $("#currentDay").text(currentDate);
-
-  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -28,4 +19,22 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  $(function () {
+  
+  });
+
+  var currentDate = dayjs().format("MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+
+
+
+
+$(".saveBtn").on("click", function () {
+
+  var userInput = $(this).siblings(".description").val();
+
+  var timeBlockId = $(this).parent().attr("id");
+
+  localStorage.setItem(timeBlockId, userInput);
 });
